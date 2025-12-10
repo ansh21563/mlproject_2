5697acbf355972cabee644280f79d5d8c3ec3314
Ansh Aggarwal <anshagg410@gmail.com>
2025-12-10 19:24:42 +0530

    sql connection established

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from src.MLPROJECT_2.logger import logging
 from src.MLPROJECT_2.exception import CustomException
+
+from src.MLPROJECT_2.components.data_ingestion import DataIngestion
+from src.MLPROJECT_2.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__=="__main__":
@@ -7,7 +10,9 @@ if __name__=="__main__":
 
 
     try:
-        a=1/0
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
       logging.info("Custom Exception")
       raise CustomException(e,sys)
